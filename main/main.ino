0f09ee050cdd8942e418f6ce5d0a16c1dafefea5
mattniemer <matthewniemer2018@u.northwestern.edu>
2017-04-27 18:51:53 -0500

    added comments

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,16 @@
+// pin constants go here
 const int pwm1 = 10; // left wheel
 const int dir1 = 11;
 const int pwm2 = 9;  // right wheel
 const int dir2 = 12;
 
+// setup functions here
 void setup() {
   // put your setup code here, to run once:
   motor_controller_setup();
 }
 
+// main loop
 void loop() {
   // put your main code here, to run repeatedly:
   drive_forward();
