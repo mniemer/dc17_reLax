9f824af9fdec2eb64ac490c696cbbaab9456dcd2
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-05-06 15:54:42 -0500

    Drive test for omni wheels
    drive_angle function
    wheels 1,3 are forward in y-direction
    wheels 2,4 are forward in x-direction
    HIGH dir is forward

--- drive_test/drive_test.ino
+++ drive_test/drive_test.ino
@@ -1,7 +1,14 @@
-const int pwm1 = 10; // left wheel
-const int dir1 = 11;
-const int pwm2 = 9;  // right wheel
-const int dir2 = 12;
+const int pwm1 = 3; // wheel 1 (forward/backward)
+const int dir1 = 4;
+const int pwm2 = 5; // wheel 2 (left/right)
+const int dir2 = 6;
+const int pwm3 = 7; // wheel 3 (forward/backward)
+const int dir3 = 8;
+const int pwm4 = 9; // wheel 4 (left/right)
+const int dir4 = 10;
+
+// const int spd = 100; // motor speed
+int drv = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,57 +16,122 @@ void setup() {
   pinMode(dir1, OUTPUT);
   pinMode(pwm2, OUTPUT);
   pinMode(dir2, OUTPUT);
-
+  pinMode(pwm3, OUTPUT);
+  pinMode(dir3, OUTPUT);
+  pinMode(pwm4, OUTPUT);
+  pinMode(dir4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  drive_forward();
+
+  /*digitalWrite(dir4, HIGH);
+  digitalWrite(dir3, HIGH);
+  digitalWrite(dir2, HIGH);
+  digitalWrite(dir1, HIGH);
+
+  analogWrite(pwm1,100);
   delay(1000);
-  drive_backward();
+  analogWrite(pwm1,0);
+  analogWrite(pwm2,100);
   delay(1000);
-  turn_left();
+  analogWrite(pwm2,0);
+  analogWrite(pwm3,100);
   delay(1000);
-  turn_right();
+  analogWrite(pwm3,0);
+  analogWrite(pwm4,100);
+  delay(1000);
+  analogWrite(pwm4,0);*/
+  
+  /*drive_angle(90, 100); //drive_forward();
   delay(1000);
   stop_moving();
+  delay(100);
+  drive_angle(0, 100); //drive_right();
   delay(1000);
+  stop_moving();
+  delay(100);
+  drive_angle(270, 100); //drive_backward();
+  delay(1000);
+  stop_moving();
+  delay(100);
+  drive_angle(180, 100); //drive_left();
+  delay(1000);
+  stop_moving();
+  delay(2000);*/
 
+  drive_angle(drv, 70);
+  delay(10);
+  ++drv;
 }
 
-void drive_forward() {
-  analogWrite(pwm1,255);
+void drive_angle(double angle, int spd) {
+  angle = (PI/180) * angle;
+  int motor1 = spd*sin(angle);
+  int motor3 = spd*sin(angle);
+  int motor2 = spd*cos(angle);
+  int motor4 = spd*cos(angle);
+  
+  digitalWrite(dir1,motor1>=0);
+  analogWrite(pwm1,abs(motor1));
+  digitalWrite(dir2,motor2>=0);
+  analogWrite(pwm2,abs(motor2));
+  digitalWrite(dir3,motor3>=0);
+  analogWrite(pwm3,abs(motor3));
+  digitalWrite(dir4,motor4>=0);
+  analogWrite(pwm4,abs(motor4));
+}
+
+void drive_forward(int spd) {
+  analogWrite(pwm1,spd);
   digitalWrite(dir1,HIGH);
-  analogWrite(pwm2,255);
-  digitalWrite(dir2,HIGH);
+  analogWrite(pwm3,spd);
+  digitalWrite(dir3,HIGH);
+
+  analogWrite(pwm2,0);
+  analogWrite(pwm4,0);
 }
 
-void drive_backward() {
-  analogWrite(pwm1,255);
+void drive_backward(int spd) {
+  analogWrite(pwm1,spd);
   digitalWrite(dir1,LOW);
-  analogWrite(pwm2,255);
-  digitalWrite(dir2,LOW);
+  analogWrite(pwm3,spd);
+  digitalWrite(dir3,LOW);
+
+  analogWrite(pwm2,0);
+  analogWrite(pwm4,0);
 }
 
-void turn_left() {
-  analogWrite(pwm1,255);
-  digitalWrite(dir1,LOW);
-  analogWrite(pwm2,255);
+void drive_left(int spd) {
+  analogWrite(pwm2,spd);
+  digitalWrite(dir2,LOW);
+  analogWrite(pwm4,spd);
+  digitalWrite(dir4,LOW);
+
+  analogWrite(pwm1,0);
+  analogWrite(pwm3,0);
+}
+
+void drive_right(int spd) {
+  analogWrite(pwm2,spd);
   digitalWrite(dir2,HIGH);
-}
+  analogWrite(pwm4,spd);
+  digitalWrite(dir4,HIGH);
 
-void turn_right() {
-  analogWrite(pwm1,255);
-  digitalWrite(dir1,HIGH);
-  analogWrite(pwm2,255);
-  digitalWrite(dir2,LOW);
+  analogWrite(pwm1,0);
+  analogWrite(pwm3,0);
 }
 
 void stop_moving() {
   analogWrite(pwm1,0);
   digitalWrite(dir1,HIGH);
+  analogWrite(pwm3,0);
+  digitalWrite(dir3,HIGH);
+
   analogWrite(pwm2,0);
   digitalWrite(dir2,HIGH);
+  analogWrite(pwm4,0);
+  digitalWrite(dir4,HIGH);
 }
 
 
