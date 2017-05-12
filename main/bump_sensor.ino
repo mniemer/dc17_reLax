edae3421b474006ad0376ce4887a6a89f4cda2a1
Andrew SaLoutos <andrewsaloutos2018@u.northwestern.edu>
2017-05-12 00:14:52 -0500

    bump sensor isr in main, not tested yet

--- main/bump_sensor.ino
+++ main/bump_sensor.ino
@@ -1,13 +1,22 @@
 // bump sensor functions defined here
+boolean bump_flag = false;
 boolean[4] bump_sensors;
 boolean retreating;
 int bump_count;
+
+void button_isr();
 
 void bump_sensor_setup() {
   pinMode(B_IN1, INPUT);
   pinMode(B_IN2, INPUT);
   pinMode(B_IN3, INPUT);
   pinMode(B_IN4, INPUT);
+
+  attachInterrupt(B_IN1, button_isr, FALLING); // interrrupt 1 is data ready
+  attachInterrupt(B_IN2, button_isr, FALLING); // interrrupt 1 is data ready
+  attachInterrupt(B_IN3, button_isr, FALLING); // interrrupt 1 is data ready
+  attachInterrupt(B_IN4, button_isr, FALLING); // interrrupt 1 is data ready
+  
   reset_bump_sensors();
   retreating = false;
 }
@@ -27,6 +36,7 @@ void reset_bump_sensors() {
   for (int i = 0; i < 4; i++) {
     bump_sensors[i] = false;
   }
+  bump_flag = false;
 }
 
 boolean check_all_bump_sensors() {
@@ -59,5 +69,32 @@ boolean retreat_after_bump() {
   }
 }
 
-
+void button_isr() {
+  if (bump_flag == false) {
+    bump_sensors[0] = 1;
+    bump_sensors[1] = 1;
+    bump_sensors[2] = 1;
+    bump_sensors[3] = 1;
+    delayMicroseconds(30);
+    for (int i=0; i<5; i++) {
+      for (int j = B_IN1; j < B_IN4+1; j++) {
+        bump_sensors[j-B_IN1] &= check_bump_sensor(j);  
+      }
+      //delayMicroseconds(5);
+    }
+    for (int j = B_IN1; j < B_IN4+1; j++) {
+        stop_moving();
+        //delayMicroseconds(1000*(j-B_IN1+1)*bump_sensors[j-B_IN1]);
+        bump_flag |= bump_sensors[j-B_IN1];
+    }
+    if (bump_flag == true) {
+      state = BUMPED;
+    }
+    /*Serial.print(bump_flag);
+    Serial.print(bump_sensors[0]);
+    Serial.print(bump_sensors[1]);
+    Serial.print(bump_sensors[2]);
+    Serial.println(bump_sensors[3]);*/
+  }
+}
 
